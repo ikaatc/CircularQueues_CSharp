e252a6d4450026229f1f09601d9a49d12523d891
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2022-12-15 16:33:24 +0700

    make class queues and declare int and array

--- CircularQueues_CSharp/Program.cs
+++ CircularQueues_CSharp/Program.cs
@@ -6,6 +6,18 @@ using System.Threading.Tasks;
 
 namespace CircularQueues_CSharp
 {
+    class Queues
+    {
+        int FRONT, REAR, max = 5;
+        int[] queue_array = new int[5];
+        public Queues()
+        {
+            /*Initializing the values of the variables REAR and FRONT to
+             -1 to indicate that the queue is initially empty.*/
+            FRONT = -1;
+            REAR = -1;
+        }
+    }
     internal class Program
     {
         static void Main(string[] args)
