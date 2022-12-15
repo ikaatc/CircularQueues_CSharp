632a71ca2430eed0819f890903307cf91a3a409d
TC1085 <ika.tania.ft21@mail.umy.ac.id>
2022-12-15 16:36:59 +0700

    make method insert in class queues

--- CircularQueues_CSharp/Program.cs
+++ CircularQueues_CSharp/Program.cs
@@ -17,6 +17,34 @@ namespace CircularQueues_CSharp
             FRONT = -1;
             REAR = -1;
         }
+        public void insert(int element)
+        {
+            /* This statement checks for the overflow condition. */
+            if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+            {
+                Console.WriteLine("\nQueue overflow\n");
+                return;
+            }
+            if (FRONT == -1)
+            {
+                FRONT = 0;
+                REAR = 0;
+            }
+            else
+            {
+                /* If REAR is at the last position of the array, then the value
+                 of REAR is set to 0 that corresponds to the first position of the array. */
+                if (REAR == max - 1)
+                    REAR = 0;
+                else
+                    /* If REAR is not the last position, then its value is
+                     increment by one. */
+                    REAR = REAR + 1;
+            }
+            /* Once the position of REAR is detemined, the element is added at
+              its proper place. */
+            queue_array[REAR] = element;
+        }
     }
     internal class Program
     {
